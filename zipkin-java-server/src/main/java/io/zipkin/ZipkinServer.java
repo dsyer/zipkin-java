dbfc8fde5ef056a90fb08bcb5db8749a501aafc7
Dave Syer <dsyer@pivotal.io>
2015-10-07 12:00:07 +0100

    Initial enhancements to Bootification

--- zipkin-java-server/src/main/java/io/zipkin/ZipkinServer.java
+++ zipkin-java-server/src/main/java/io/zipkin/ZipkinServer.java
@@ -13,51 +13,44 @@
  */
 package io.zipkin;
 
+import static java.util.Collections.emptyList;
+
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
+import org.jooq.conf.Settings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 import com.facebook.swift.codec.ThriftCodecManager;
 import com.facebook.swift.service.ThriftServer;
 import com.facebook.swift.service.ThriftServerConfig;
 import com.facebook.swift.service.ThriftServiceProcessor;
-import com.zaxxer.hikari.HikariDataSource;
+
 import io.zipkin.jdbc.JDBCSpanStore;
 import io.zipkin.scribe.Scribe;
 import io.zipkin.scribe.ScribeSpanConsumer;
-import java.io.IOException;
-import org.jooq.conf.Settings;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import static io.zipkin.internal.Util.envOr;
-import static java.util.Collections.emptyList;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ZipkinServerProperties.class)
 public class ZipkinServer {
 
-  public static void main(String[] args) throws IOException, InterruptedException {
-    // Silence Invalid method name: '__can__finagle__trace__v3__'
-    System.setProperty("logging.level.com.facebook.swift.service.ThriftServiceProcessor", "OFF");
+  @Autowired
+  ZipkinServerProperties server;
 
-    System.setProperty("server.port", envOr("QUERY_PORT", "9411"));
+  public static void main(String[] args) throws IOException, InterruptedException {
     SpringApplication.run(ZipkinServer.class, args)
         .getBean(ThriftServer.class).start();
   }
 
   @Bean
-  SpanStore provideSpanStore() {
+  SpanStore provideSpanStore(DataSource datasource) {
     if (System.getenv("MYSQL_HOST") != null) {
-      String mysqlHost = System.getenv("MYSQL_HOST");
-      int mysqlPort = envOr("MYSQL_TCP_PORT", 3306);
-      String mysqlUser = envOr("MYSQL_USER", "");
-      String mysqlPass = envOr("MYSQL_PASS", "");
-
-      String url = String.format("jdbc:mysql://%s:%s/zipkin?user=%s&password=%s&autoReconnect=true",
-          mysqlHost, mysqlPort, mysqlUser, mysqlPass);
-
-      HikariDataSource datasource = new HikariDataSource();
-      datasource.setDriverClassName("com.mysql.jdbc.Driver");
-      datasource.setJdbcUrl(url);
-      datasource.setMaximumPoolSize(10);
-      datasource.setConnectionTestQuery("SELECT '1'");
       return new JDBCSpanStore(datasource, new Settings());
     } else {
       return new InMemorySpanStore();
@@ -71,10 +64,28 @@ public class ZipkinServer {
 
   @Bean
   ThriftServer scribeServer(Scribe scribe) {
-    int scribePort = envOr("COLLECTOR_PORT", 9410);
     ThriftServiceProcessor processor = new ThriftServiceProcessor(new ThriftCodecManager(), emptyList(), scribe);
     return new ThriftServer(processor, new ThriftServerConfig()
         .setBindAddress("localhost")
-        .setPort(scribePort));
+        .setPort(this.server.getCollector().getPort()));
+  }
+}
+
+@ConfigurationProperties("zipkin")
+class ZipkinServerProperties {
+  private Collector collector = new Collector();
+  public Collector getCollector() {
+    return this.collector;
+  }
+  static class Collector {
+    private int port = 9410;
+
+    public int getPort() {
+      return this.port;
+    }
+
+    public void setPort(int port) {
+      this.port = port;
+    }
   }
 }
